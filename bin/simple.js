6cbc56e88d2665630e94759e9b005a476466ff18
Tyler Flint <tylerflint@gmail.com>
2020-10-16 15:09:21 -0400

    Initial workflow carved out

--- bin/simple.js
+++ bin/simple.js
@@ -4,78 +4,70 @@
 const app = require('commander')
 
 // import the commands
+const init      = require('../lib/cmd/init')
 const dev       = require('../lib/cmd/dev')
-const build     = require('../lib/cmd/build')
 const deploy    = require('../lib/cmd/deploy')
 const teardown  = require('../lib/cmd/teardown')
-const logs      = require('../lib/cmd/logs')
-const migration = require('../lib/cmd/migration')
+const test      = require('../lib/cmd/test')
 const migrate   = require('../lib/cmd/migrate')
-const doctor    = require('../lib/cmd/doctor')
+const migration = require('../lib/cmd/migration')
+const wrangler  = require('../lib/cmd/wrangler')
+
+// set the defaults
+app
+  .option('-c, --config <path>', 'Config path, defaults to ./simple.config.js', `${process.cwd()}/simple.config.js`)
+
+// init serverless-simple
+app
+  .command('init')
+  .description('Initialize a project with serverless-simple')
+  .action(init)
+
+// create serverless-simple app
 
 // dev
 app
   .command('dev')
   .description('Run and develop the app, locally')
-  .action(function () {
-    dev()
-  })
+  .action(dev)
 
-// build
+// test
 app
-  .command('build')
-  .description('Ensure the app compiles')
-  .action(function () {
-    build()
-  })
+  .command('test')
+  .description('Run the test suite')
+  .action(test)
 
 // deploy
 app
-  .command('deploy')
+  .command('deploy [env]')
   .description('Deploy the app live')
-  .action(function () {
-    deploy()
-  })
+  .action(deploy)
 
 // teardown
 app
   .command('teardown')
   .description('Tear down a deployment')
-  .action(function () {
-    teardown()
-  })
-
-// logs
-app
-  .command('logs')
-  .description('Connect to a deployment and stream logs')
-  .action(function () {
-    logs()
-  })
+  .action(teardown)
 
 // migration
 app
   .command('migration')
-  .description('Create a database migration')
-  .action(function () {
-    migration()
-  })
+  .description('Generate a migration from template')
+  .action(migration)
 
 // migrate
 app
   .command('migrate')
   .description('Run pending migrations')
-  .action(function () {
-    migrate()
-  })
+  .action(migrate)
   
-// doctor
+// wrangler
 app
-  .command('doctor')
-  .description('Verify installation and configuration')
-  .action(function () {
-    doctor()
-  })
-
+  .command('wrangler [subcommand...]')
+  .description('Run wrangler command within serverless-simple environment')
+  .allowUnknownOption(true)
+  .storeOptionsAsProperties(true)
+  .action(wrangler)
+  
 // run the app!
-app.parse(process.argv)
+app.parseAsync(process.argv)
